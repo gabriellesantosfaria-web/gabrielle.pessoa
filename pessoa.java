f3152b864073939ed2ab6bffef6f2bf9708f29c3
gabriellesantosfaria-web <gabrielle.santos.faria@escola.pr.gov.br>
2026-08-12 12:10:01 -0300

    pessoa.java

---
+++ pessoa.java
@@ -0,0 +1,16 @@
+class cliente extends pessoa{
+
+    public class Pessoa {
+       
+       String nome;
+       int idade;
+
+       public Pessoa(string nome , int idade){
+         this.nome=nome; 
+          this.idade=idade;
+        } 
+        public void apresentar(){ 
+          system.out.println("Nome:"+nome+"idade:"+ idade);
+        }    
+    }
+}
