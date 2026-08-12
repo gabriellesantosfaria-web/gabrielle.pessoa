a154fed9fa9061c90cd64e6aa9009e5f905747d3
gabriellesantosfaria-web <gabrielle.santos.faria@escola.pr.gov.br>
2026-08-12 12:06:32 -0300

    aluno.js

---
+++ aluno.js
@@ -0,0 +1,11 @@
+class aluno extends pessoa{
+      constructor(Nome, idade){
+        this.nome= nome; 
+        this.idade= idade;
+      }
+      apresentar(){
+        console.log(`nome:${this.nome} | idade:${this.idade} | CGM:${this.cgm}`);
+      }
+    }
+    const pessoa=new pessoa("João,20");
+    pessoa.apresentar();
